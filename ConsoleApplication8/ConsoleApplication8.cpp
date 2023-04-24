2ae0652bbfeecd4e9a0e3f4a132320bfc79c5055
DNS <DNS@DESKTOP-UGIN9D2>
2023-04-24 23:58:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	char a;
+	cout << "Введите символ:\n";
+	cin >> a;
+	if (a >= '0' && a <= '9') {
+		cout << "Вы ввели цифрую." << endl;
+	}
+	else if (a >= 'A' && a <= 'Z') {
+		cout << "Вы ввели заглавную английскую букву." << endl;
+	}
+	else if (a >= 'a' && a <= 'z') {
+		cout << "Вы ввели строчную английскую букву." << endl;
+	}
+	else if (a == '.' || a == ',' || a == '"' || a == ';' || a == ':' || a == '?' || a == '!') {
+		cout << "Вы ввели знак препинания.";
+	}
+
+	return 0;
+}
